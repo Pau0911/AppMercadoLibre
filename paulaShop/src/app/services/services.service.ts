231379a561dce72133ae028f01c77d589d984ed2
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2020-09-16 22:43:34 -0500

    routing

--- paulaShop/src/app/services/services.service.ts
+++ paulaShop/src/app/services/services.service.ts
@@ -8,20 +8,19 @@ import { map } from 'rxjs/operators';
 export class ServicesService {
 
   url ="https://api.mercadolibre.com/sites/MCO/search?q="
-  urluser="https://api.mercadolibre.com/user/"
+  urlProduct="https://api.mercadolibre.com/items/"
   constructor(private http: HttpClient) {     
 
   }
 
   getProduct(product: string, offset:number) {
-  
     return this.http.get(`${this.url}${product}&offset=${offset}`).pipe(map((data: any) => {
       console.log("data",data)
       return data;
     }));
   }
-  getSeller(idSeller: string){
-    return this.http.get(`${this.urluser}${idSeller}`).pipe(map((data: any) => {
+  getIdProduct(idproduct: string){
+    return this.http.get(`${this.urlProduct}${idproduct}`).pipe(map((data: any) => {
       return data;
     }));
   }
