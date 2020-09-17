b5b215d41f2f53c578812aa8cdb641f83198478e
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2020-09-16 21:58:43 -0500

    routing

--- paulaShop/src/app/app-routing.module.ts
+++ paulaShop/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'search',component:NavbarComponent},
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
