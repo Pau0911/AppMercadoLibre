e24cfe783ec43ff4bdadae0c3516ac066e505aa6
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2020-10-13 14:28:04 -0500

    prod

--- paulaShop/server.js
+++ paulaShop/server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/paulaShop'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/paulaShop'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/paulaShop' }
-  );
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/paulaShop/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
